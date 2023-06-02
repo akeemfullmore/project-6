0d5deaf52d9de422e44d4c0ad289dc437e4ad6b6
akeemfullmore <123107932+akeemfullmore@users.noreply.github.com>
2023-06-02 10:52:33 -0400

    variables

---
+++ app.js
@@ -0,0 +1,9 @@
+const qwerty = document.getElementById('qwerty');
+const phrase = document.getElementById('phrase');
+const missed = 0;
+const startBtn = document.getElementsByClassName('btn_reset');
+
+startBtn.addEventListener('click', (e) => {
+    console.log(startBtn);
+
+});
